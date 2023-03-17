a39ecbf7665b01ab46c98066ddbf68e1a1745bab
brightkingpin <luvandabright781@gmail.com>
2023-03-17 20:07:07 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+// Error 1
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+// Error 2
 
-// Write your code here
+const num3 = 16
+const num4 = 6
+
+const mod = num3 % num4
+
+const max = Math.max(5, 10, 15, 20)
+
+const random = Math.floor(Math.random() * 100) + 1
